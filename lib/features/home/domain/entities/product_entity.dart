9b382a91ddd115f2593f7556c9947a0d81f17d1a
Marwan Ali Abdelkader <marwanalizz42@gmail.com>
2024-07-12 19:00:55 +0300

    implement product entity

---
+++ lib/features/home/domain/entities/product_entity.dart
@@ -0,0 +1,9 @@
+class ProductEntity {
+  final String productImage;
+  final String productTitle;
+  final String productDescription;
+  final num productdiscountPercentage;
+  final num productRatring;
+
+  ProductEntity({required this.productImage, required this.productTitle, required this.productDescription, required this.productdiscountPercentage, required this.productRatring});
+}
