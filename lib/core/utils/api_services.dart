bcd21232cdbb94563b7f4ac11f155da12b158f8c
Marwan Ali Abdelkader <marwanalizz42@gmail.com>
2024-07-13 06:30:10 +0300

    implement remote fetch products

--- lib/core/utils/api_services.dart
+++ lib/core/utils/api_services.dart
@@ -4,7 +4,7 @@ class ApiServices {
    final String _baseUrl = "https://dummyjson.com/";
    final Dio _dio = Dio();
   ApiServices(Dio dio);
-  Future<Map<String, dynamic>> get({required String endPoint}) async {
+  Future<Map<String, dynamic>> getHomeProducts({required String endPoint}) async {
     var response = await _dio.get("$_baseUrl$endPoint");
     return response.data;
   }
--- lib/features/home/data/data_sources/home_remote_data_source.dart
+++ lib/features/home/data/data_sources/home_remote_data_source.dart
@@ -1,19 +1,23 @@
 import 'package:route_attendence_task/core/utils/api_services.dart';
+import 'package:route_attendence_task/features/home/data/models/product_model/product_model.dart';
 import 'package:route_attendence_task/features/home/domain/entities/product_entity.dart';
 
 abstract class HomeRemoteDataSource {
-  Future< List<ProductEntity>> fetchProducts();
+  Future<List<ProductEntity>> fetchProducts();
 }
 
 class HomeRemoteDataSourceImpl implements HomeRemoteDataSource {
   final ApiServices apiServices;
 
   HomeRemoteDataSourceImpl(this.apiServices);
-  
-  @override
-  Future<List<ProductEntity>> fetchProducts() {
-  
-    throw UnimplementedError();
-  }
 
+  @override
+  Future<List<ProductEntity>> fetchProducts() async {
+    var data = await apiServices.getHomeProducts(endPoint: 'products');
+    List<ProductEntity> products = [];
+    for (var productMap in data['products']) {
+      products.add(ProductModel.fromJson(productMap));
+    }
+    return products;
+  }
 }
