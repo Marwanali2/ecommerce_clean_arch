f93b2becbf22ef1b103b2a55eba45ca434524d76
Marwan Ali Abdelkader <marwanalizz42@gmail.com>
2024-07-13 05:31:33 +0300

    create home remote data source

--- lib/features/home/data/data_sources/home_remote_data_source.dart
+++ lib/features/home/data/data_sources/home_remote_data_source.dart
@@ -1,44 +1,19 @@
-import 'package:hive/hive.dart';
 import 'package:route_attendence_task/core/utils/api_services.dart';
+import 'package:route_attendence_task/features/home/domain/entities/product_entity.dart';
 
 abstract class HomeRemoteDataSource {
-  // Future<List<BookEntity>> fetchFeaturedBooksDomain({int pageNumber=0});
-  // Future<List<BookEntity>> fetchBestBooksDomain();
+  Future< List<ProductEntity>> fetchProducts();
 }
 
 class HomeRemoteDataSourceImpl implements HomeRemoteDataSource {
   final ApiServices apiServices;
 
   HomeRemoteDataSourceImpl(this.apiServices);
+  
+  @override
+  Future<List<ProductEntity>> fetchProducts() {
+  
+    throw UnimplementedError();
+  }
 
-  // @override
-  // Future<List<BookEntity>> fetchFeaturedBooksDomain({int pageNumber=0}) async{
-  //   var featuredBooksMapDomain = await apiServices.get(
-  //     endPoint:
-  //     "volumes?Filtering=free-ebooks&q=subject:programming&Sorting=newest&startIndex=${pageNumber*10}",
-  //   );
-  //   List<BookEntity>featuredBooksListDomain=getBooksList(featuredBooksMapDomain);
-  //   saveDataToBox(featuredBooksListDomain,kFeaturedBooks);
-  //   return featuredBooksListDomain;
-  // }
-
-  // @override
-  // Future<List<BookEntity>> fetchBestBooksDomain()async {
-
-  //     var bestBooksMabDomainData = await apiServices.get(
-  //       endPoint: "volumes?Filtering=free-ebooks&q=subject:flowers",
-  //     );
-  //     List<BookEntity> bestSellerBooksListDomain = getBooksList(bestBooksMabDomainData);
-  //     saveDataToBox(bestSellerBooksListDomain,kBestBooks);
-  //     return bestSellerBooksListDomain;
-
-  // }
-
-  // List<BookEntity> getBooksList(Map<String, dynamic> data) {
-  //   List<BookEntity> books = [];
-  //   for (var bookMap in data['items']) {
-  //     books.add(BookModel.fromJson(bookMap));
-  //   }
-  //   return books;
-  // }
 }
