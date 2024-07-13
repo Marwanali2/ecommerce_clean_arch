983a2b79f5817bf69bd11227fee0c9fec5b0914c
Marwan Ali Abdelkader <marwanalizz42@gmail.com>
2024-07-13 03:00:57 +0300

    refactor fetch product use case

--- lib/features/home/domain/use_cases/fetch_products_use_case.dart
+++ lib/features/home/domain/use_cases/fetch_products_use_case.dart
@@ -1,13 +1,16 @@
 import 'package:dartz/dartz.dart';
 import 'package:route_attendence_task/core/errors/failure.dart';
+import 'package:route_attendence_task/core/use_cases/use_case.dart';
 import 'package:route_attendence_task/features/home/domain/entities/product_entity.dart';
 import 'package:route_attendence_task/features/home/domain/repos/home_repo.dart';
 
-class FetchProductsUseCase {
+class FetchProductsUseCase extends UseCase<List<ProductEntity>, NoParam> {
   final HomeRepo homeRepo;
 
   FetchProductsUseCase(this.homeRepo);
-  Future<Either<Failure, List<ProductEntity>>> fetchProducts() {
-    return homeRepo.fetchProducts();
+
+  @override
+  Future<Either<Failure, List<ProductEntity>>> call([NoParam? param]) async {
+    return await homeRepo.fetchProducts();
   }
 }
