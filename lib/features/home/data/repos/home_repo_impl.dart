89d36caca11f2a8c1f975e1997603b512d583045
Marwan Ali Abdelkader <marwanalizz42@gmail.com>
2024-07-14 00:01:01 +0300

    implement home repo implementaion

--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -1,57 +1,29 @@
 import 'dart:core';
-
 import 'package:dartz/dartz.dart';
-import 'package:dio/dio.dart';
 import 'package:route_attendence_task/core/errors/failure.dart';
 import 'package:route_attendence_task/features/home/data/data_sources/home_local_data_source.dart';
 import 'package:route_attendence_task/features/home/data/data_sources/home_remote_data_source.dart';
+import 'package:route_attendence_task/features/home/domain/entities/product_entity.dart';
+import 'package:route_attendence_task/features/home/domain/repos/home_repo.dart';
 
-// class HomeRepoImpl extends HomeRepoDomain {
-//   final HomeRemoteDataSource homeRemoteDataSource;
-//   final HomeLocalDataSource homeLocalDataSource;
+class HomeRepoImpl extends HomeRepo {
+  final HomeLocalDataSource homeLocalDataSource;
+  final HomeRemoteDataSource homeRemoteDataSource;
 
-//   HomeRepoImpl(
-//       {required this.homeRemoteDataSource, required this.homeLocalDataSource});
-
-//   // @override
-//   // Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooksDomain(
-//   //     {int pageNumber = 0}) async {
-//   //   try {
-//   //     var featuredBooksListLocal =
-//   //         homeLocalDataSource.fetchFeaturedBooksLocalDomain(
-//   //           pageNumber: pageNumber
-//   //         );
-//   //     if (featuredBooksListLocal.isNotEmpty) {
-//   //       return right(featuredBooksListLocal);
-//   //     }
-//   //     var featuredBooksListRemote =
-//   //         await homeRemoteDataSource.fetchFeaturedBooksDomain(pageNumber: pageNumber);
-//   //     return right(featuredBooksListRemote);
-//   //   } catch (e) {
-//   //     if (e is DioException) {
-//   //       return left(ServerFailure(errorMessage: e.toString()));
-//   //     } else {
-//   //       return left(
-//   //         ServerFailure(
-//   //           errorMessage: e.toString(),
-//   //         ),
-//   //       );
-//   //     }
-//   //   }
-//   // }
-
-//   // @override
-//   // Future<Either<Failure, List<BookEntity>>> fetchBestBooksDomain() async {
-//   //   try {
-//   //     var bestBooksLocal = homeLocalDataSource.fetchBestBooksLocalDomain();
-//   //     if (bestBooksLocal.isNotEmpty) {
-//   //       return right(bestBooksLocal);
-//   //     }
-//   //     var bestBooksRemote = await homeRemoteDataSource.fetchBestBooksDomain();
-//   //     return right(bestBooksRemote);
-//   //   } catch (e) {
-//   //     return left(ServerFailure(errorMessage: e.toString()));
-//   //   }
-//   // }
-
-// }
+  HomeRepoImpl(
+      {required this.homeLocalDataSource, required this.homeRemoteDataSource});
+  @override
+  Future<Either<Failure, List<ProductEntity>>> fetchProducts() async {
+    try {
+      List<ProductEntity> productsList;
+      productsList = homeLocalDataSource.fetchProducts();
+      if (productsList.isNotEmpty) {
+        return right(productsList);
+      }
+       productsList = await homeRemoteDataSource.fetchProducts();
+      return right(productsList);
+    } on Exception catch (e) {
+      return left(ServerFailure(errorMessage: e.toString()));
+    }
+  }
+}
