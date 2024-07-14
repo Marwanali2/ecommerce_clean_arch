977ddc58238c35df4e2d893b22e755d6603769cc
Marwan Ali Abdelkader <marwanalizz42@gmail.com>
2024-07-14 05:20:16 +0300

    implement bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:route_attendence_task/core/Dependency_injection/service_locator.dart';
 import 'package:route_attendence_task/core/utils/constants.dart';
+import 'package:route_attendence_task/core/utils/simple_bloc_observer.dart';
 import 'package:route_attendence_task/features/home/domain/entities/product_entity.dart';
 import 'package:route_attendence_task/route_task.dart';
 
@@ -10,6 +12,6 @@ void main() async {
   Hive.registerAdapter(ProductEntityAdapter());
   await Hive.openBox<ProductEntity>(kProductsBox);
   setupServiceLocator();
+  Bloc.observer = SimpleBlocObserver();
   runApp(const RouteTask());
 }
-
